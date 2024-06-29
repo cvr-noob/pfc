7a74c5fcb4af08acf6e1e1c1ac380b9be7e11df7
cvr-noob <srichaitanyavictims2023@gmail.com>
2024-06-29 17:17:16 +0530

    Create lineCountWordCount.py

---
+++ lineCountWordCount.py
@@ -0,0 +1,12 @@
+with open('file.txt', 'w') as file:
+    file.write("""Hello World
+    Eswar""")
+
+fp=open('file.txt', 'r')
+l=0
+w=0
+for line in fp:
+    w+=len(line.split())
+    l+=1
+print(f'{l} lines and {w} words')    
+fp.close()
