8bc26e466c191a15f6076537d11d4b78490923e9
cvr-noob <srichaitanyavictims2023@gmail.com>
2024-06-29 17:21:27 +0530

    Create months.py

---
+++ months.py
@@ -0,0 +1,5 @@
+months = ['January', 'February', 'March', 'April', 'May', 'June', 'July',
+          'August', 'September', 'October', 'November', 'December']
+
+n = int(input("Enter month number: "))
+print(months[n - 1])
